dfb7a17bac4db76c38966807bbe7c190fa59210c
Hrithik <slumdog4321@gmail.com>
2023-09-17 12:31:51 +0530

    skill section added in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import ProjectSection from "@/components/ProjectSection";
+import SkillSection from "@/components/SkillSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <ProjectSection />
+      <SkillSection />
     </>
   );
 }
